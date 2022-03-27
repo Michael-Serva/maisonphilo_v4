9225ce3b696c8ff921bd3c4821608d22c28a9247
Michael Serva <servam95@gmail.com>
2022-03-27 11:18:57 +0200

    test

--- src/Security/RegistrationAuthenticator.php
+++ src/Security/RegistrationAuthenticator.php
@@ -21,7 +21,7 @@ class RegistrationAuthenticator extends AbstractLoginFormAuthenticator
 
     public const LOGIN_ROUTE = 'app_login';
 
-    private UrlGeneratorInterface $urlGenerator;
+    private $urlGenerator;
 
     public function __construct(UrlGeneratorInterface $urlGenerator)
     {
