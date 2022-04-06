0084455cd90616bac5c5b8a1c3d8744da71ed1f9
Michael Serva <servam95@gmail.com>
2022-04-06 08:32:49 +0200

    test

--- assets/scripts/selectPath.js
+++ assets/scripts/selectPath.js
@@ -3,7 +3,7 @@ let country = document.querySelector("#list");
 if (country) {
   country.addEventListener("click", function () {
     if (this.value == 0) {
-      
+      return
     }else window.location.pathname = "/country/" + this.value;
   });
 }
