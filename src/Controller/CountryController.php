fc6e6569fada54e1b1a8587c27c5f83985d859df
Michael Serva <servam95@gmail.com>
2022-05-01 09:51:55 +0200

    test

--- src/Controller/CountryController.php
+++ src/Controller/CountryController.php
@@ -7,6 +7,7 @@ use App\Form\CountryType;
 use App\Repository\CountryRepository;
 use App\Repository\HospitalRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -15,7 +16,35 @@ use Symfony\Component\Routing\Annotation\Route;
  */
 class CountryController extends AbstractController
 {
-  
+    /**
+     * @Route("/", name="app_country_index", methods={"GET"})
+     */
+    public function index(CountryRepository $countryRepository): Response
+    {
+        return $this->render('country/index.html.twig', [
+        ]);
+    }
+
+    /**
+     * @Route("/new", name="app_country_new", methods={"GET", "POST"})
+     */
+    public function new(Request $request, CountryRepository $countryRepository): Response
+    {
+        $country = new Country();
+        $form = $this->createForm(CountryType::class, $country);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $countryRepository->add($country);
+            return $this->redirectToRoute('app_country_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('country/new.html.twig', [
+            'country' => $country,
+            'form' => $form,
+        ]);
+    }
+
     /**
      * @Route("/{code}", name="app_country_show", methods={"GET"})
      */
@@ -25,5 +54,36 @@ class CountryController extends AbstractController
             'country' => $country,
             'hospitals' => $hospitalRepository->hospitalShow($country)
         ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="app_country_edit", methods={"GET", "POST"})
+     */
+    public function edit(Request $request, Country $country, CountryRepository $countryRepository): Response
+    {
+        $form = $this->createForm(CountryType::class, $country);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $countryRepository->add($country);
+            return $this->redirectToRoute('app_country_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('country/edit.html.twig', [
+            'country' => $country,
+            'form' => $form,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="app_country_delete", methods={"POST"})
+     */
+    public function delete(Request $request, Country $country, CountryRepository $countryRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete' . $country->getId(), $request->request->get('_token'))) {
+            $countryRepository->remove($country);
+        }
+
+        return $this->redirectToRoute('app_country_index', [], Response::HTTP_SEE_OTHER);
     }
 }
