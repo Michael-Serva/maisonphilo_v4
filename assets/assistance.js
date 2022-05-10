6a87ef8110953055f834f0dd952eed610690df53
Michael Serva <servam95@gmail.com>
2022-05-10 20:02:38 +0200

    test

--- assets/assistance.js
+++ assets/assistance.js
@@ -7,15 +7,9 @@ import "./scripts/selectPath";
 import $ from "jquery";
 
 $(document).ready(function () {
-  $("#partner").click(function (e) { 
+  $(".hospital").on("click", function (e) { 
     e.preventDefault();
-    e.$("#partner").toggleClass("bg-secondary text-light");
-    e.$(".partnerShow").toggleClass("d-none");
-        
-  });
-  $("#hospital").click(function (e) { 
-    e.preventDefault();
-    $("#hospital").toggleClass("bg-secondary text-light");
-    $(".hospitalShow").toggleClass("d-none");
+    $(this).toggleClass("bg-secondary text-light");
+    $(this).$(".showDescription").toggleClass("d-none");
   });
 });
