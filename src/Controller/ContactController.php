d7d7d6f37a858f058a38b03a74397abc9b9766af
Michael Serva <servam95@gmail.com>
2022-05-28 22:11:05 +0200

    test

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -4,14 +4,25 @@ namespace App\Controller;
 
 use App\Entity\Contact;
 use App\Form\ContactType;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+/**
+ * @Template
+ * @Route("/contact")
+ */
 class ContactController extends AbstractController
 {
-    #[Route('/contact', name: 'app_contact_index')]
+    /**
+     * Undocumented function
+     *
+     * @param Request $request
+     * @return Response
+     * @Route("/")
+     */
     public function index(
         Request $request
     ): Response
