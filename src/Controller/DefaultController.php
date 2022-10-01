cc8e454cfa0503e1c9a4e29c755c2fba1a087cdd
Michael Serva <servam95@gmail.com>
2022-10-01 17:35:12 +0200

    test

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\ContactRepository;
+use App\Repository\SurveyRepository;
 use Symfony\Component\Serializer\Serializer;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -17,8 +18,11 @@ class DefaultController extends AbstractController
     #[Route('/default', name: 'app_default')]
     public function index(
         ContactRepository $contactRepository,
+        SurveyRepository $surveyRepository,
         ContactSerializer $serializer
     ): Response {
+
+        /* recuperation des  messages decontacts en format xml */
         $contact = $contactRepository->findAll();
 
         $encoders = [new XmlEncoder(), new JsonEncoder()];
@@ -27,6 +31,12 @@ class DefaultController extends AbstractController
 
         dump($serializer->serialize($contact, "xml"));
 
+        /* Recuperation des donnes du formulaire en xml */
+
+        $survey = $surveyRepository->findAll();
+
+        dump($serializer->serialize($survey, "xml"));
+        
         return $this->render('default/index.html.twig', [
             'controller_name' => 'DefaultController',
         ]);
