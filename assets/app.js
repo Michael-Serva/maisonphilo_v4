582e3996d60086a0c2a6b73e0a5e8feefe5ad930
Michael Serva <servam95@gmail.com>
2022-10-16 15:12:55 +0200

    effet slide et toggle

--- assets/app.js
+++ assets/app.js
@@ -27,8 +27,8 @@ $(function () {
   for (let i = 1; i <= 3; i++) {
     $(`.show${i}`).on("click", function (e) {
       e.preventDefault();
-      $(`#show${i}`).toggleClass("d-none");
-      $(`.hide${i}`).toggleClass("d-none");
+      $(`#show${i}`).fadeToggle().toggleClass("d-none");
+      $(`.hide${i}`).fadeToggle().toggleClass("d-none");
     });
   }
 });
